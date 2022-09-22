273852d66d2e8ef78f10c1ef3479016dd01a0fc2
leesh0829 <lsh77392125@gmail.com>
2022-09-22 21:11:37 +0900

    2022_09_22

---
+++ ConsoleApp1/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Player
+
+
+
+    {
+        public int hp = 100;
+        public int at = 10;
+
+        public void AddAt(int _at)
+        {
+            at += _at;
+        }
+
+        //public void AddAt() //void는 리턴을 안해주겠다는 거
+        //{
+        //at += 10;
+        //공격력을 올려주기 위한 함수
+        //}
+
+        
+        public void Fight(int monsterAt)
+        {
+           hp -= monsterAt;
+        }
+        internal void Addat(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args) //Main 은 제일 먼저 실행되는 코드 방
+        {
+            Player player = new Player();
+            Monster monster1 = new Monster();
+
+            player.AddAt(10);
+            //Player at;
+            player.Fight(monster1.at);
+
+            Console.WriteLine(monster1.at);
+            Console.WriteLine(player.at);
+
+            //player.Addat(10);
+
+            //player.at += 10;
+            Console.ReadLine();
+
+        }
+}
+    
+}
